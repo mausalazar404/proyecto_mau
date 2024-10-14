d2eeb41440d49988f5a5c413557ea4051b5a735e
mausalazar404 <a01712976@tec.mx>
2024-10-14 13:15:56 -0600

    Update Examen.py

--- Examen.py
+++ Examen.py
@@ -1,30 +1,37 @@
-import random
+import random 
+"""Se importó la libreria de python llamada "random". Esta 
+proporciona funciones para generar números pseudoaleatorios y realizar 
+acciones aleatorias"""
 
 
 caracteres = int(input("número de caracteres para la contraseña (9 o más): "))
-
+"""Se asigna el valor a los caracteres"""
 
 
 def crear_numero():
     return str(random.randint(0,9))
+"Esta función sirve para devolver numeros aleatorios del 0 al 9"
 
 def crear_min():
     letras = "abcdefghijklmnopqrstuvwxyz"
     a = random.randint(0, (len(letras)-1))
     return letras[a]
+"Esta función genera letras aleatorias en minúscula"
 
 def crear_may():
     letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     a = random.randint(0, (len(letras)-1))
     return letras[a]
+"Esta funcion crea genera las letras en mayúscula"
 
 def crear_simb():
     simbolos = "!#$%&/()=?¡;,.:-_"
     a = random.randint(0, (len(simbolos)-1))
     return simbolos[a]
+"Esta función genera los simbolos aleatoriamente"
 
 funciones = [crear_numero,crear_min,crear_may,crear_simb]
-
+"""Se crea una variable que llama a las funciones anteriores"""
 
 def generar_contrasena(longitud_c, funciones):
     contrasena = ""
@@ -64,11 +71,17 @@ def guardar_contrasena(contrasena_final):
         else:
             print("Contraseña final: ", contrasena_final)
             break
+"""Se crea una función para guardar la contraseña. Pregunta si quieres guardar
+la contraseña, si le pones que sí, se crea una lista para la cual te pide un
+nombre. La función agrega la contraseña a una lista y esa lista a la lista
+de guardado. Finalmente te pide si quieres crear una contraseña nueva para 
+guardarla en el espacio de guardado. Si le pones que no"""
 
 guardar_contrasena(contrasena_final)
+"""Se manda a llamar a la función"""
 
 print("Gracias :)")
-
+"""Se imprime un "Gracias" """
         
 
 
